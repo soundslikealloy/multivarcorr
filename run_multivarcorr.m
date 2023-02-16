69b934e1e4bccca38c792ed97afa6127e49fc29e
soundslikealloy <eloi.mrp@gmail.com>
2023-02-16 09:11:44 +0000

    version 1

---
+++ run_multivarcorr.m
@@ -0,0 +1,25 @@
+function run_multivarcorr(data_file)
+    %%%%% Main function to run multi-variable correlation analysis
+    % data_name (string): name of file (with the extension)
+
+    addpath(genpath('lib')); % make every subfolder with functions accessible to the code
+    
+    %% Read data and create results structure
+    data = loadData(data_file);
+    
+    %% Run multi-varaible correlation script
+    dataset = data.values;
+    [~, d] = size(dataset);
+    [RKtau, RKt_pval, n, deltas, relyRange, symbolMatrix_up, symbolMatrix_down] = multivarcorr(d, dataset);
+
+    %% Display results
+    fprintf('> Dimensions: %i\n', d)
+    fprintf('> Reliable range limit: %.4f\n', relyRange)
+    fprintf('> Number of observations: %i\n', n)
+    
+    fprintf('\n>> List of coefficients Data trends | δ coefficients | τ coefficients (p-value) \n')
+    for i = 1:2^d/2
+        fprintf("%s%s | %.4f | %.4f (%.4f)\n", symbolMatrix_up(i,:), symbolMatrix_down(i,:), deltas(i), RKtau(i), RKt_pval(i))
+    end
+    fprintf('\n')
+end
